5f7486fa2398368f46a10a991343f63119a0e9b9
RahulGautamSingh <rahultesnik@gmail.com>
2022-10-26 15:52:41 +0530

    refactor: remove types BranchName and CommitSha

--- lib/util/git/types.ts
+++ lib/util/git/types.ts
@@ -76,22 +76,6 @@ export interface CommitFilesConfig {
   platformCommit?: boolean;
 }
 
-export type BranchName = string;
-export type TargetBranchName = BranchName;
-export type SourceBranchName = BranchName;
-
-export type GitConflictsCache = Record<TargetBranchName, TargetBranchConflicts>;
-
-export interface TargetBranchConflicts {
-  targetBranchSha: CommitSha;
-  sourceBranches: Record<SourceBranchName, SourceBranchConflict>;
-}
-
-export interface SourceBranchConflict {
-  sourceBranchSha: CommitSha;
-  isConflicted: boolean;
-}
-
 export interface CommitResult {
   parentCommitSha: string;
   commitSha: string;
